1c451b57756fd095500f3ab82adcdf81985b3049
Kristian Hellang <kristian@hellang.com>
2015-07-22 20:33:39 +0200

    Initial commit

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var list = new List<int>();
+        }
+    }
+}
